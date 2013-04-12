c978cc9b9b0c14e36d307c403603b26b6c2fb8e9
Joao Paulo <joaosilvafa@gmail.com>
2013-04-12 01:21:56 -0300

    Organizado tela de produto e adicionado database

--- gen/com/example/projetoandroid/R.java
+++ gen/com/example/projetoandroid/R.java
@@ -24,21 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
+        public static final int action_settings=0x7f08000f;
         public static final int button1=0x7f080007;
         public static final int editText1=0x7f080006;
         public static final int imgProduto=0x7f080000;
         public static final int imgProdutoDetalhes=0x7f080009;
         public static final int precoMax=0x7f080005;
         public static final int precoMaxDtl=0x7f08000c;
-        public static final int precoMaxDtlText=0x7f08000b;
+        public static final int precoMaxDtlText=0x7f08000a;
         public static final int precoMaxText=0x7f080004;
         public static final int precoMin=0x7f080003;
         public static final int precoMinDtl=0x7f08000d;
-        public static final int precoMinDtlText=0x7f08000a;
         public static final int precoMinText=0x7f080002;
+        public static final int precominDtlText=0x7f08000b;
         public static final int produto=0x7f080001;
         public static final int textProdutosDetalhes=0x7f080008;
+        public static final int textView1=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_busca=0x7f030000;
